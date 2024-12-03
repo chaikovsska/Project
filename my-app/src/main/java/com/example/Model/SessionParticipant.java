b22319403806c87df53b9f58a20e27af92eecfd4
Chaikovska Sofiia <145142859+chaikovsska@users.noreply.github.com>
2024-12-03 16:12:48 +0200

    Update SessionParticipant.java

--- my-app/src/main/java/com/example/Model/SessionParticipant.java
+++ my-app/src/main/java/com/example/Model/SessionParticipant.java
@@ -1,20 +1,17 @@
 package com.example.Model;
 
 public class SessionParticipant {
-    private int sessionId;      // Ідентифікатор сесії
-    private int participantId;  // Ідентифікатор учасника
+    private int sessionId;      
+    private int participantId;  
 
-    // Конструктор за замовчуванням (потрібний для DAO)
     public SessionParticipant() {
     }
 
-    // Конструктор з параметрами sessionId та participantId
     public SessionParticipant(int sessionId, int participantId) {
         this.sessionId = sessionId;
         this.participantId = participantId;
     }
 
-    // Гетери та сетери для sessionId та participantId
     public int getSessionId() {
         return sessionId;
     }
